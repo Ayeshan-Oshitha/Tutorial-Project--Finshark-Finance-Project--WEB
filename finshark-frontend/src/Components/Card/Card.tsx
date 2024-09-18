a4b63b1bc7f2853c82d5954f4a4f082852d24c5e
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2024-09-18 16:00:27 +0530

    3. Props

--- finshark-frontend/src/Components/Card/Card.tsx
+++ finshark-frontend/src/Components/Card/Card.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import './Card.css'
 
-type Props = {}
+interface Props {
+    companyName : string;
+    ticker : string;
+    price : number
+}
 
-const Card = (props: Props) => {
+const Card = ({companyName , ticker, price}: Props) => {
   return (
     
     <>
     <div className="card">
         <img src="https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F8ed3d547-94ff-48e1-9f20-8c14a7030a02_2000x2000.jpeg" />
         <div className="details">
-            <h2>AAPL</h2>
-            <p>$110</p>
+            <h2>{companyName} ({ticker})</h2>
+            <p>${price}</p>
         </div>
-        <p className="infon">
+        <p className="info">
             Lorem Ipsum
         </p>
     </div>
---
+++ finshark-frontend/src/Components/CardList/CardList.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from '../Card/Card'
+
+interface Props { }
+
+const CardList = (props: Props) => {
+    return (
+        <>
+            <Card companyName='Apple' ticker='AAPL' price={100} />
+            <Card companyName='Microsoft' ticker='MSFT' price={125}/>
+            <Card companyName='Tesla' ticker='TSLA' price={300}/>
+
+        </>
+    )
+}
+
+export default CardList
